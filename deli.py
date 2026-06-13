d172843ed637950bff7265dbc7341ef31f68d87d
Dakota Nagy <dnagy6@student.cscc.edu>
2026-06-13 14:44:14 -0400

    Implement while loop to process orders and move to finished list

--- deli.py
+++ deli.py
@@ -1,3 +1,15 @@
 # Initial lists for the deli orders
 sandwich_orders = ['Tuna', 'Ham', 'Veggie', 'Turkey', 'Chicken', 'Roast Beef']
 finished_sandwiches = []
+
+# Process each sandwich order until there are none left
+while sandwich_orders:
+    # Remove the first sandwich order from the list
+    current_sandwich = sandwich_orders.pop(0)
+    
+    # Simulate making the sandwich
+    print(f"I made your {current_sandwich} sandwich.")
+    
+    # Add the finished sandwich to the finished_sandwiches list
+    finished_sandwiches.append(current_sandwich)
+    
